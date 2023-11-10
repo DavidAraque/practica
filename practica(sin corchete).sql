b2030da9607bf3959f5d143187d45f81881b03ed
DavidAraque <69062553+DavidAraque@users.noreply.github.com>
2023-11-10 12:17:14 -0300

    Update practica(sin corchete).sql

--- practica(sin corchete).sql
+++ practica(sin corchete).sql
@@ -39,4 +39,35 @@ CREATE TABLE Estudiante (
     matricula INT PRIMARY KEY,
     rut_estudiante NVARCHAR(50) NOT NULL UNIQUE,
     nombre_estudiante NVARCHAR(50) NOT NULL,
+    apellido_estudiante NVARCHAR(50) NOT NULL,
+    correo_estudiante NVARCHAR(50) NOT NULL,
+    carrera NVARCHAR(50) NOT NULL,
+    sede NVARCHAR(50) NOT NULL,
+    estadomatricula NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Turno (
+    id_turno INT AUTO_INCREMENT PRIMARY KEY,
+    numero_atencion NVARCHAR(50) NOT NULL UNIQUE,
+    matriculaid INT NOT NULL,
+    especialidadid INT NOT NULL,
+    fecha_hora DATETIME NOT NULL,
+    CONSTRAINT FK_Turno_Estudiante FOREIGN KEY (matriculaid) REFERENCES Estudiante (matricula),
+    CONSTRAINT FK_Turno_Especialidad FOREIGN KEY (especialidadid) REFERENCES Especialidad (id_especialidad)
+);
+
+CREATE TABLE Turno_estadoturno (
+    id_turno_estadoturno INT AUTO_INCREMENT PRIMARY KEY,
+    turnoid INT NOT NULL,
+    estadoturnoid INT NOT NULL,
+    empleadoid INT NOT NULL,
+    fecha DATETIME NOT NULL,
+    CONSTRAINT FK_Turno_estadoturno_Turno FOREIGN KEY (turnoid) REFERENCES Turno (id_turno),
+    CONSTRAINT FK_Turno_estadoturno_Estadoturno FOREIGN KEY (estadoturnoid) REFERENCES Estadoturno (id_estado_turno),
+    CONSTRAINT FK_Turno_estadoturno_empleado FOREIGN KEY (empleadoid) REFERENCES Empleado (id_empleado)
+);
+
+    matricula INT PRIMARY KEY,
+    rut_estudiante NVARCHAR(50) NOT NULL UNIQUE,
+    nombre_estudiante NVARCHAR(50) NOT NULL,
     apellido_estudiante NVARCHAR(50) NOT NULL
